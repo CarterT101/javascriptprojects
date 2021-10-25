32fbccaea65163d46335a758be1475d897695d10
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2021-10-25 11:23:27 -0700

    Learning JavaScript

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+var a="cpu"; b="gpu"; c="motherboard"; d="ram"; e="storage"; //assigning each variable a value
+var a = a.fontcolor("blue"); //assigning each variable a fontcolor
+var b = b.fontcolor("green");
+var c = c.fontcolor("red");
+var d = d.fontcolor("purple");
+var e = e.fontcolor("yellow");
+document.write(a+b+c+d+e); //printing each variable into a string
+
+var x = 10; //assigning a variable value
+var y = (x + 5); //writing an expression
+document.write(y); //printing result
