fe63116d1e6a8552f2ef57e3d2b09ccd5b80ac96
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2021-10-25 11:30:00 -0700

    updated main.js

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,11 +1,18 @@
+window.alert('This is a practice assignment');
+
 var a="cpu"; b="gpu"; c="motherboard"; d="ram"; e="storage"; //assigning each variable a value
 var a = a.fontcolor("blue"); //assigning each variable a fontcolor
 var b = b.fontcolor("green");
 var c = c.fontcolor("red");
 var d = d.fontcolor("purple");
 var e = e.fontcolor("yellow");
-document.write(a+b+c+d+e); //printing each variable into a string
+
+var first = "This is one part and then "; //first part of string
+var second = "this is the other half"; //second part of string
+document.write(first + second); //concatenating the strings
 
 var x = 10; //assigning a variable value
 var y = (x + 5); //writing an expression
-document.write(y); //printing result
+
+
+
