50b7085aa4810e7cf47d48bffd5e0f7950449242
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2021-10-26 09:43:57 -0700

    Added comments

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -1,13 +1,13 @@
 function add(num1, num2) {
-        var num1 = parseInt(document.getElementById("num1").value);
-        var num2 = parseInt(document.getElementById("num2").value);
-        var answ = document.getElementById("answer");
-        answ.value = num1 + num2;
+        var num1 = parseInt(document.getElementById("num1").value); //brings first inputted number from line
+        var num2 = parseInt(document.getElementById("num2").value); //brings second inputted number from line 2
+        var answ = document.getElementById("answer"); //makes answ equal to the input box with the id of "answer"
+        answ.value = num1 + num2; //finds answer by taking the two inputted numbers by adding them together
 }
 
 function concatenate() {
-        var sentence = "I guess you";
-        sentence += " get a surprise.";
-        document.getElementById("words").innerHTML = sentence;
+        var sentence = "I guess you"; //defines variable sentence
+        sentence += " get a surprise."; //adds words to the variable sentence
+        document.getElementById("words").innerHTML = sentence; //replaces id "words" innerHTML with the sentence variable by making them equal to each other
 
 }
