3bd59f574af3e04003fb314e2263e6afaf096e24
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2021-10-26 12:51:25 -0700

    New Assigments
    Made new index.html and main.js as well as modified older ones and did a lot of new challenges assigned to me with new functions and methods

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -5,9 +5,17 @@ function add(num1, num2) {
         answ.value = num1 + num2; //finds answer by taking the two inputted numbers by adding them together
 }
 
+function multiply(num1, num2) {
+        var num1 = parseInt(document.getElementById("num1").value);
+        var num2 = parseInt(document.getElementById("num2").value);
+        var answ = document.getElementById("answer");
+        answ.value = num1 * num2;
+}
+
 function concatenate() {
         var sentence = "I guess you"; //defines variable sentence
         sentence += " get a surprise."; //adds words to the variable sentence
         document.getElementById("words").innerHTML = sentence; //replaces id "words" innerHTML with the sentence variable by making them equal to each other
 
 }
+
---
+++ Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,50 @@
+function addition() {
+    var addition = 5 + 5; //making variable
+    document.getElementById("add").innerHTML = "5 + 5 = " + addition; //replacing id with this and adding the variable onto the string
+}
+
+function subtraction() {
+    var subtraction = 5 - 2;
+    document.getElementById("subtract").innerHTML = "5 - 2 = " + subtraction;
+}
+
+function multiplydivide() {
+    var multiply = 3 * 5;
+    var divide = 15/3;
+    document.getElementById("multdivide").innerHTML = "3 * 5 = " + multiply + ". 15/3 = " + divide;
+}
+
+function simplemath() {
+    var math = (5 + 2 - 3) * 3 / 6;
+    document.getElementById("simplemath").innerHTML = "Five plus two subtracted by three then multiplied by three and divided by six equals = " + math;
+}
+
+function remainder() {
+    var remainder = 30%4;
+    document.getElementById("remainder").innerHTML = "The remainder of 30 divided by 4 is = " + remainder;
+}
+
+function operator() {
+    var operate = parseInt(document.getElementById("operator").value); //creating variable and making the string an integer 
+    var answeroperate = document.getElementById("answeroperator"); //setting answer equal to ID 
+    answeroperate.value = -operate; //changing operator sign 
+}
+
+function increment() {
+    var inc = parseInt(document.getElementById("increase").value); //setting variable inc equal to ID increase value
+    document.getElementById("increase").value = ++inc; //setting ID increase equal to inc value + 1 
+}
+
+function decrement() {
+    var dec = parseInt(document.getElementById("decrease").value); 
+    document.getElementById("decrease").value = --dec; //setting ID decrease equal to dec value - 1
+}
+
+function randomnumber() {
+    random = Math.random() * 1000; //sets variable random to a random number from 0-1000
+    document.getElementById("random").innerHTML = parseInt(random); //sets ID equal to variable random and makes it an integer, so no decimal points
+}
+
+function round() {
+    document.getElementById("round").value = Math.round(document.getElementById("round").value);
+}
