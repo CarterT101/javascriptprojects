76b32d22f36fe41d250805c8c74ba5e3b9954132
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2021-10-28 13:03:25 -0700

    updated main.js

--- Basic_JavaScript_Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic_JavaScript_Projects/Project9_countdown_slideshow/JS/main.js
@@ -31,18 +31,18 @@ function currentslide(n) {
     showSlides(slideIndex = n); //changes current slide on dot button click
 }
 
-function showSlides(n) { //don't understand
+function showSlides(n) { 
     var i; //creates i variable
     var slides = document.getElementsByClassName("Slides"); //grabs Slides class and sets it equal to variable slides
     var dots = document.getElementsByClassName("dots"); //creates dots variable and sets it equal to dots class
-    if (n > slides.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    if (n > slides.length) {slideIndex = 1} //if n parameter is greater than total amount of slides, in this case 4, sets slideindex = 1
+    if (n < 1) {slideIndex = slides.length} // if n parameter is less than 1, sets index to slides length, in this case 4. These two lines are for making sure it will stay in the amount of slides and pictures there are
+    for (i = 0; i < slides.length; i++) { // sets i = 0, checks if it is less than total slide length, and then add ones. 
+        slides[i].style.display = "none"; // makes sure only one slide shows up at a time
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    for (i = 0; i < dots.length; i++) { //sets i equal to 0, checks if it is less than dot amount, and then adds one. 
+        dots[i].className = dots[i].className.replace(" active", ""); //removes "active" from other dots, ensures that not all of the dots are highlighted
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex-1].style.display = "block"; //displays slide at current slide index
+    dots[slideIndex-1].className += " active"; //adds active class to the dot at current slide index
 }
